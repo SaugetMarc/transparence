e945eff83977adac9b6a24a91ad275d3663dd47b
Marc Sauget <marc@durnick.home>
2021-03-14 22:02:23 +0100

    first

---
+++ basics/management/commands/transparent.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = 'Génére excel formater à partir de la base transparence'
+
+    def add_arguments(self, parser):
+        parser.add_argument('nb_entreprise', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+
+        self.stdout.write(self.style.SUCCESS('Fichier géneré'))
