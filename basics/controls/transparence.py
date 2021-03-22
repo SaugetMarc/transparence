0249ed5badefead08d0d31b6d85ca14d500a0f50
Marc Sauget <marc@durnick.home>
2021-03-22 21:37:56 +0100

    premier formatage

--- basics/controls/transparence.py
+++ basics/controls/transparence.py
@@ -1,9 +1,17 @@
 from  ..services.loader import Loader
-from pandas import merge
+from pandas import merge, ExcelWriter
+from datetime import datetime
 
 class Transparence():
 
+
+
+    def genere_excel_writer(self, name):
+        return ExcelWriter(name +str(datetime.now().strftime("_%Y%m%d")) + '.xlsx', engine='xlsxwriter')
+
+
     def control(self):
+        writer = self.genere_excel_writer("transparence")
         self.loader = Loader()
 
         self.loader.loadcsv()
@@ -16,18 +24,23 @@ class Transparence():
 
         #calcul de la rémunération (avantage)
 
-        self.loader.avantages = self.loader.convert_row(self.loader.avantages, "avant_montant_ttc")
+        #self.loader.avantages = self.loader.convert_row(self.loader.avantages, "avant_montant_ttc")
         
 
-        #merge de la table des entreprises pour la table des avantages
-
-        print(self.loader.avantages[["entreprise_identifiant"]])
-        print(self.loader.entreprises[["identifiant"]])
+        #merge de la table des entreprises pour la table des avantages        
         data_merge = merge(self.loader.avantages, self.loader.entreprises, how="left", left_on=["entreprise_identifiant"], right_on="identifiant")
-        print(data_merge)
+        data = data_merge.groupby(["secteur","entreprise_identifiant"])["avant_montant_ttc"].sum().head(50)
+        data.to_excel(writer, sheet_name="Avantage")
 
-        data = data_merge.groupby(["secteur","entreprise_identifiant"])["avant_montant_ttc"].sum()
-        print(data)
+        worksheet = writer.sheets["Avantage"]
+        format_size = 'C2:C'+str(data.shape[0]+1)        
+        worksheet.conditional_format(format_size, {'type': '3_color_scale'})
+        worksheet.set_column('A:C', 20)
+        
+
+        
+
+        writer.save()
 
 
         
--- basics/services/loader.py
+++ basics/services/loader.py
@@ -14,10 +14,10 @@ class Loader():
     def loadcsv(self):
         #fichier entreprise
         print(settings.DATA)
-        self.entreprises  = read_csv(settings.DATA['entreprise'], header=0, delimiter=',', encoding='iso-8859-15', dtype=str, error_bad_lines=False)
-        self.avantages  = read_csv(settings.DATA['avantage'], header=0, delimiter=';', encoding='iso-8859-15', dtype=str, error_bad_lines=False)
-        self.conventions  = read_csv(settings.DATA['convention'], header=0, delimiter=';', encoding='iso-8859-15', dtype=str, error_bad_lines=False)
-        self.remunerations  = read_csv(settings.DATA['remuneration'], header=0, delimiter=';', encoding='iso-8859-15', dtype=str, error_bad_lines=False)
+        self.entreprises  = read_csv(settings.DATA['entreprise'], header=0, delimiter=',', dtype=str, error_bad_lines=False)
+        self.avantages  = read_csv(settings.DATA['avantage'], header=0, delimiter=';', error_bad_lines=False)
+        self.conventions  = read_csv(settings.DATA['convention'], header=0, delimiter=';', dtype=str, error_bad_lines=False)
+        self.remunerations  = read_csv(settings.DATA['remuneration'], header=0, delimiter=';',  dtype=str, error_bad_lines=False)
 
         return True
 
